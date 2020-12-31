e990b996fbc973aeb719748f47aad5fe4e511f7a
Simon <sijeecodes@gmail.com>
2021-01-01 01:02:08 +0900

    UPDATE to show all elements tagged by tag filter

--- src/utils/filterByTags.js
+++ src/utils/filterByTags.js
@@ -2,24 +2,17 @@ const filterByTags = (tree, tags, tagFilter) => {
   let result = [];
   let tagIds = Object.keys(tags);
 
-  for(let i = 0; i < tagIds.length; i++) {
-    let matched = 0;
-
-    tagFilter.map(el => {
-      if(tags[tagIds[i]].indexOf(el) !== -1) {
-        matched++;
-      }
-      return null;
-    });
-
-    if(matched === tagFilter.length){
-      for(let j = 0; j < tree.length; j++) {
-        if(tree[j].id === tagIds[i]) {
-          result.push(tree[j]);
+  tree.forEach(el => {
+    if(tagIds.indexOf(el.id) !== -1) {
+      for(let i = 0; i < tagFilter.length; i++) {
+        if(tags[el.id].indexOf(tagFilter[i]) !== -1) {
+          result.push(el);
+          return null;
         }
       }
     }
-  }
+  });
+
   return result;
 }
 
